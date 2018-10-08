0ca1b7670766a836f78fa74834c27a8ff4071dbb
nagarajnaidu <nagarajnaidu921@gmail.com>
2018-10-08 17:06:05 +0530

    Update user-auth.service.ts

--- src/app/services/auth/user-auth.service.ts
+++ src/app/services/auth/user-auth.service.ts
@@ -14,25 +14,13 @@ export class UserAuthService {
   ) { }
 
   login(data) {
-    const body = new URLSearchParams();
-    body.set('username', data.username);
-    body.set('password', data.password);
-    body.set('output_mode', 'json');
-    const options = {
-      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
-
-  };
-
-  console.log(body.toString());
-
-      this.http.post('https://localhost:8089/services/auth/login', body.toString(), options).subscribe(
-        (resData) => {
-          console.log(resData);
-        },
-        (err) => {
-          console.log(err);
-        }
-      );
     console.log(data);
+    this.http.post('http://localhost:3000/auth/login', data)
+    .subscribe(resData => {
+      console.log(resData);
+    },
+    (err)=>{
+      console.log(err);
+    })
     }
 }
